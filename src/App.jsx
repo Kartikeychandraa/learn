2b2535d46ea09d44dd73962af100ee2bddc74325
Kartikeychandra <40001828+Kartikeychandraa@users.noreply.github.com>
2022-06-17 04:34:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="m">
+      <table>
+      <tr>
+      <td><button>-</button>
+      </td>
+        <td><button>-</button></td>
+        <td><button>-</button></td>
+      </tr>
+        <tr>
+      <td><button>-</button>
+      </td>
+        <td><button>-</button></td>
+        <td><button>-</button></td>
+      </tr>
+        <tr>
+      <td><button>-</button>
+      </td>
+        <td><button>-</button></td>
+        <td><button>-</button></td>
+      </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
